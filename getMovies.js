4f16409439d04c92ef5f4e76cab44892a025de71
Shanshan Chen <shanshanc.chen@gmail.com>
2019-07-09 16:35:01 +0800

    feat: use https get

--- getMovies.js
+++ getMovies.js
@@ -1,59 +1,45 @@
 const https = require('https');
 
 function getMovieTitles(substr) {
-    const BASE_URL = 'https://jsonmock.hackerrank.com/api/movies/search/?Title='
-    const url = BASE_URL + substr;
-    let movies = [];
-    let total_pages = 0;
-    const getMovies = url => {
+    // https request function
+    const getMovies = (url) => {
       return new Promise((resolve, reject) => {
         https.get(url, res => {
           res.setEncoding('utf8');
           let body = [];
           res.on('data', chunk => {
             body.push(chunk);
-            // console.log('chunk: ', chunk);
           });
           res.on('end', () => resolve(body.join('')));
         }).on('error', reject);
       });
     };
-    
-    let titles = [];
-    const data = getMovies(url)
-      .then(movies => {
-        let obj = JSON.parse(movies);
-        const total_pages = obj.total_pages;
+
+    const promise1 = getMovies('https://jsonmock.hackerrank.com/api/movies/search/?Title=Spiderman');
+    promise1.then(res => {
+      // check the number of total pages
+      const posStart = res.indexOf('"total_pages"')+14;
+      const posEnd = res.indexOf(',', posStart);
+      const totalPage = Number(res.substring(posStart, posEnd));
+
+      let urlList = [];
+      for (let i = 1; i <= totalPage; i++) {
+        urlList.push(getMovies(`https://jsonmock.hackerrank.com/api/movies/search/?Title=Spiderman&page=${i}`));
+      }
+      // resolve all promises
+      Promise.all(urlList).then(items => {
+        let titles = [];
+        items.forEach(item => {
+          const movies = JSON.parse(item).data;
+          for (let movie of movies) {
+            titles.push(movie.Title);
+          }
+        });
+        console.log('titles: ', titles, 'total: ', titles.length);
         
-        for (let movie of obj.data) {
-          titles.push(movie.Title);
-        }
-
-
-        console.log('movie titles: ', titles);
-        return titles
-      })
-      .catch(err => console.error(err));
-
-    return data;
-    // console.log('body: ', body);
-    // return body;
-    // const results = https.get(url, res => {
-    //   let content = '';
-      
-    //   res.on('data', chunk => {
-    //     content += chunk;
-    //   });
-
-    //   res.on('end', () => {
-    //     total_pages = JSON.parse(content).total_pages;
-    //     movies = JSON.parse(content).data;
-    //     return movies;
-    //   })    
-
-    // }).on('error', err => console.error(err.message));
-    // return results;
+      }).catch(e => console.error(e));
+    })
+   
 };
 
-// console.log(getMovieTitles('Spiderman'));
 getMovieTitles('Spiderman');
